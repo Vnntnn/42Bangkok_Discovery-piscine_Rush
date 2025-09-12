9bb4f2156cd18490e0a3c4ef0fd566fbb999ad57
Asujiikaruda <jirapon.vongyaichim@gmail.com>
2025-09-12 23:32:14 +0700

    what the heck

---
+++ guport/script.js
@@ -0,0 +1,9 @@
+// เอฟเลื่อน
+document.querySelectorAll('.navbar a').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
